d5f12a86374757b1d4692a74cf5c5a02fc518aba
Alpha Olomi <alphaolomi@gmail.com>
2019-08-01 02:09:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
 // Connect to mongo
-mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
-    if(err){
+const url = "mongodb://localhost:27017/mongochat";
+mongo.connect(url, { useNewUrlParser: true }, function (err, db) {
+    if (err) {
         throw err;
     }
 
     console.log('MongoDB connected...');
 
     // Connect to Socket.io
-    client.on('connection', function(socket){
+    client.on('connection', function (socket) {
         let chat = db.collection('chats');
 
         // Create function to send status
-        sendStatus = function(s){
+        sendStatus = function (s) {
             socket.emit('status', s);
         }
 
         // Get chats from mongo collection
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-            if(err){
+        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) {
                 throw err;
             }
 
@@ -29,17 +30,17 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
         });
 
         // Handle input events
-        socket.on('input', function(data){
+        socket.on('input', function (data) {
             let name = data.name;
             let message = data.message;
 
             // Check for name and message
-            if(name == '' || message == ''){
+            if (name == '' || message == '') {
                 // Send error status
                 sendStatus('Please enter a name and message');
             } else {
                 // Insert message
-                chat.insert({name: name, message: message}, function(){
+                chat.insert({ name: name, message: message }, function () {
                     client.emit('output', [data]);
 
                     // Send status object
@@ -52,9 +53,9 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
         });
 
         // Handle clear
-        socket.on('clear', function(data){
+        socket.on('clear', function (data) {
             // Remove all chats from collection
-            chat.remove({}, function(){
+            chat.remove({}, function () {
                 // Emit cleared
                 socket.emit('cleared');
             });
