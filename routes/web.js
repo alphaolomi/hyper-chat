6ddc5c26ea3891d19941312e82039429aca4c3f7
Alpha Olomi <alphaolomi@gmail.com>
2019-10-22 23:06:35 +0300

    WIP: add register page

--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 
-const registerController = require("../controllers/register");
-const loginController = require("../controllers/login");
-const homeController = require("../controllers/home");
+const registerController = require('../controllers/register');
+const loginController = require('../controllers/login');
+const homeController = require('../controllers/home');
 
 const router = express.Router();
 
@@ -14,7 +14,6 @@ router.get('/login', loginController.showLoginForm);
 router.post('/login', loginController.login);
 
 router.get('/register', registerController.showRegistrationForm);
-// router.post('/register', registerController.registerUser);
-
+router.post('/register', registerController.registerUser);
 
 module.exports = router;
