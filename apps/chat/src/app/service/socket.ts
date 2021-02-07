9308f9dae9b6d26df5ad8d1eee9e9dcb0a08e7d9
Alpha Olomi <alphaolomi@gmail.com>
2021-02-07 12:26:13 +0300

    wip

---
+++ apps/chat/src/app/service/socket.ts
@@ -0,0 +1,12 @@
+import {Manager,Socket} from 'socket.io-client'
+// import ioclient from 'socket.io-client'
+
+// ioclient()
+const m = new Manager("")
+let io = new Socket(m,"")
+// // export const socket = io.connect(SOCKET_URL);
+// // elsewhere
+// // import {socket} from "service/socket";
+
+// import socketIOClient from "socket.io-client";
+// const socket = socketIOClient(ENDPOINT);
