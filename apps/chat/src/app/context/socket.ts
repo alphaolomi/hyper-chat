594c11f5ea1df294b5bfae61e0824421c79a403a
Alpha Olomi <alphaolomi@gmail.com>
2021-02-25 16:10:40 +0300

    Update socket.ts

--- apps/chat/src/app/context/socket.ts
+++ apps/chat/src/app/context/socket.ts
@@ -1,10 +1,15 @@
+import React from 'react';
+import { io, Socket } from 'socket.io-client';
 
 
-import React from 'react'
-import { io,Socket } from 'socket.io-client'
+const SOCKET_URL = "https://hyper-chat-app.herokuapp.com"
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:3333";
+// const SOCKET_URL =
+//   process.env.NODE_ENV !== 'production'
+//     ? 'http://localhost:3333'
+//     : process.env.REACT_APP_SOCKET_URL ||
+//       'https://hyper-chat-app.herokuapp.com';
 
-export const socket = io(SOCKET_URL, {})
+export const socket = io(SOCKET_URL, {});
 
-export const SocketContext = React.createContext<Socket|null>(null);
+export const SocketContext = React.createContext<Socket | null>(null);
